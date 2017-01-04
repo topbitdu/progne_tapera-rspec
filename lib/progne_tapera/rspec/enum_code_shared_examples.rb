46b69142f65ec73407b017a0299be72afe5df730
Topbit Du <topbit.du@gmail.com>
2017-01-04 13:13:34 +0800

    1, Progne Tapera Enum Code shared examples. 2, Deprecate the Enum Code shared examples.

--- lib/progne_tapera/rspec/enum_code_shared_examples.rb
+++ lib/progne_tapera/rspec/enum_code_shared_examples.rb
@@ -1,4 +1,58 @@
+shared_examples 'ProgneTapera::EnumCode' do |model, attribute_name, enum|
+
+  describe "##{attribute_name}" do
+    it 'exists' do expect(model).to respond_to(attribute_name) end
+  end
+
+  describe "##{attribute_name}=" do
+
+    it 'exists' do expect(model).to respond_to("#{attribute_name}=") end
+
+    enum.each do |enum_item|
+      it "should be able to assign #{enum_item.inspect} and load by ##{attribute_name}" do
+        model.send "#{attribute_name}=", enum_item
+        expect(model.send attribute_name).to eq(enum_item)
+      end
+      it "should be able to assign #{enum_item.inspect} and load by ##{attribute_name}_code" do
+        model.send "#{attribute_name}=", enum_item
+        expect(model.send "#{attribute_name}_code").to eq(enum_item.code)
+      end
+    end
+
+  end
+
+  describe "##{attribute_name}_code" do
+
+    it 'exists' do expect(model).to respond_to("#{attribute_name}_code") end
+
+  end
+
+  describe "##{attribute_name}_code=" do
+
+    it 'exists' do expect(model).to respond_to("#{attribute_name}_code=") end
+
+    enum.each do |enum_item|
+      it "should be able to assign #{enum_item.code.inspect} and load by ##{attribute_name}" do
+        model.send "#{attribute_name}_code=", enum_item.code
+        expect(model.send attribute_name).to eq(enum_item)
+      end
+      it "should be able to assign #{enum_item.code.inspect} and load by ##{attribute_name}_code" do
+        model.send "#{attribute_name}_code=", enum_item.code
+        expect(model.send "#{attribute_name}_code").to eq(enum_item.code)
+      end
+    end
+
+  end
+
+end
+
+
+
+# The shared examples are deprecated.
+
 shared_examples 'enum code' do |code_count, enum_list|
+
+  warn 'The "enum code" shared examples is deprecated and will be removed since v1.0, please use "ProgneTapera::EnumConfig" instead.'
 
   describe '.enum_name' do
     it 'exists' do expect(described_class).to respond_to(:enum_name) end
