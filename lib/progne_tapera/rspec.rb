e9f0e0ba4520f21bbf9df378791f6489f158e84a
Topbit Du <topbit.du@gmail.com>
2017-01-05 13:17:35 +0800

    1, Improve the RSpec module to require the Enum Config shared examples.

--- lib/progne_tapera/rspec.rb
+++ lib/progne_tapera/rspec.rb
@@ -1,6 +1,7 @@
 require "progne_tapera/rspec/version"
 
 require 'progne_tapera/rspec/enum_code_shared_examples'
+require 'progne_tapera/rspec/enum_config_shared_examples'
 require 'progne_tapera/rspec/code_attribute_shared_examples'
 
 module ProgneTapera
