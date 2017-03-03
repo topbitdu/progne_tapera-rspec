824dc074ec12cb90f5c7c3c9805dd99db8bb5eef
Topbit Du <topbit.du@gmail.com>
2017-03-03 10:05:38 +0800

    1, Improve the RSpec manifest for the coding style.

--- lib/progne_tapera/rspec.rb
+++ lib/progne_tapera/rspec.rb
@@ -1,4 +1,4 @@
-require "progne_tapera/rspec/version"
+require 'progne_tapera/rspec/version'
 
 require 'progne_tapera/rspec/enum_code_shared_examples'
 require 'progne_tapera/rspec/enum_config_shared_examples'
