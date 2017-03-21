c65325e4ad714aaca7aa14b9657749e1c00f360d
Topbit Du <topbit.du@gmail.com>
2017-03-21 16:08:34 +0800

    1, Improve the RSpec manifest to remove the Code Attribute shared examples.

--- lib/progne_tapera/rspec.rb
+++ lib/progne_tapera/rspec.rb
@@ -2,7 +2,6 @@ require 'progne_tapera/rspec/version'
 
 require 'progne_tapera/rspec/enum_code_shared_examples'
 require 'progne_tapera/rspec/enum_config_shared_examples'
-require 'progne_tapera/rspec/code_attribute_shared_examples'
 
 module ProgneTapera
   module RSpec
